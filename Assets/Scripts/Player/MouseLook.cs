5ab5fce31792c7ac73c5d8ba7c0511194e906b40
Primoz Pecar <primoz.pecar@outfit7.com>
2018-11-17 10:27:33 +0100

    Initial commit

---
+++ Assets/Scripts/Player/MouseLook.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour {
+    public GameObject Player;
+    public float LookSpeed = 2.0f;
+    private Vector2 Rotation = Vector2.zero;
+    
+    private float MinimumYDeg = -20.0f;
+    private float MaximumYDeg = 40.0f;
+
+    void Update () {
+        Rotation.x += Input.GetAxis("Mouse X");
+        Rotation.y += Input.GetAxis("Mouse Y");
+        Debug.LogFormat("b4 x: {0}, y: {1}", Rotation.x, Rotation.y);
+        Rotation.y = Mathf.Clamp(Rotation.y, MinimumYDeg, MaximumYDeg);
+        Debug.LogFormat("aftr x: {0}, y: {1}", Rotation.x, Rotation.y);
+        transform.localEulerAngles = new Vector3(0, Rotation.x * LookSpeed, Rotation.y * LookSpeed);
+    }
+}
---
+++ Assets/Scripts/Player/MovermentController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovermentController : MonoBehaviour {
+    void Start() {
+
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.W)) {
+
+        }
+        if (Input.GetKeyDown(KeyCode.S)) {
+
+        }
+        if (Input.GetKeyDown(KeyCode.A)) {
+
+        }
+        if (Input.GetKeyDown(KeyCode.D)) {
+
+        }
+        if (Input.GetKeyDown(KeyCode.Space)) {
+
+        }
+    }
+}
